be6e9afa66f2de7c6adbf232882560b695991046
Soffan Marsus Ahmad <soffanma@gmail.com>
2020-09-24 05:36:15 +0700

    update

--- app/src/main/java/com/soffanma/pertemuan1mobile/MainActivity.java
+++ app/src/main/java/com/soffanma/pertemuan1mobile/MainActivity.java
@@ -78,14 +78,14 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        switch (item.getItemId()) {
-//            // Respond to the action bar's Up/Home button
-//            case android.R.id.home:
-//                NavUtils.navigateUpFromSameTask(this);
-//                return true;
-//        }
-//        return super.onOptionsItemSelected(item);
-//    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
